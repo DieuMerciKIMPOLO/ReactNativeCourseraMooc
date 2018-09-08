e487b97c88b0672dda28d0f639c9fb2980c88ded
Dieu Merci KIMPOLO <dieumerci.k.nkokolo@aims-senegal.org>
2018-09-08 01:29:09 +0000

    update

---
+++ components/AboutComponent.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import { Text, ScrollView, View } from 'react-native';
+import { Card} from 'react-native-elements';
+import { ListItem } from 'react-native-elements';
+import { LEADERS } from '../shared/leaders';
+
+class About extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            leaders:LEADERS
+        }
+    }
+    static navigationOptions = {
+        title: 'About Us',
+    };
+    render() {
+        
+        return(
+            <ScrollView>
+                      <Card title="Our Hitory">
+                        <Text>
+                        Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
+                        </Text>
+                        <Text>
+                        Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.
+                        </Text>
+                      </Card>
+                      <Card title="Corporate Leadership">
+                        {
+                            this.state.leaders.map((item, index)=>{
+                                return(
+                                <ListItem
+                                    key={index}
+                                    title={item.name}
+                                    subtitle={item.description}
+                                    leftAvatar={{ source: require('./images/alberto.png')}}
+                                  />
+                                )
+                            })
+                        }
+                      </Card>
+            </ScrollView>
+        );
+    }
+}
+
+export default About;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
-import { View ,Platform} from 'react-native';
-import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
+import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 
 const MenuNavigator=createStackNavigator({
@@ -44,7 +46,32 @@ const HomeNavigator = createStackNavigator({
     headerTintColor: "#fff"  
   })
 });
-
+const ContactNavigator =createStackNavigator({
+  Contact: { screen: Contact }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+const AboutNavigator =createStackNavigator({
+  About: { screen: About }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
 const MainNavigator = createDrawerNavigator({
   Home: 
     { screen: HomeNavigator,
@@ -53,28 +80,34 @@ const MainNavigator = createDrawerNavigator({
         drawerLabel: 'Home'
       }
     },
+  About: 
+    { screen: AboutNavigator,
+      navigationOptions: {
+        title: 'About',
+        drawerLabel: 'About Us'
+      }
+    },
   Menu: 
-    { screen: MenuNavigator,
+    { 
+      screen: MenuNavigator,
       navigationOptions: {
         title: 'Menu',
         drawerLabel: 'Menu'
       }, 
-    }
+    },
+  Contact:{
+    screen: ContactNavigator,
+    navigationOptions: {
+      title: 'Contact',
+      drawerLabel: 'Contact Us'
+    }, 
+  }
 }, {
 drawerBackgroundColor: '#D1C4E9'
 });
 
 class Main extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //       dishes: DISHES,
-  //       selectedDish: null
-  //     };
-  // }
-  // onDishSelect(dishId) {
-  //   this.setState({selectedDish: dishId})
-  //  }
+
   render() {
  
     return (
@@ -87,10 +120,3 @@ class Main extends Component {
   
 export default Main;
 
-
-// import React, {Component} from 'react';
-// import Menu from './MenuComponent';
-// import {DISHES} from '../dishes/dishes';
-
-// <Menu dishes={this.state.dishes} onPress={(dishId) => this.onDishSelect(dishId)} />
-// <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
